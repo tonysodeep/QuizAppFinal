d47d38978a5cd41222b4871126999ac94a7b6ab4
tonysodeep <hynguyen2727@gmail.com>
2021-12-31 19:31:44 +0700

    init notification

---
+++ app/src/main/java/com/example/quizappfinal/services/PushNotificationService.java
@@ -0,0 +1,8 @@
+package com.example.quizappfinal.services;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class PushNotificationService extends FirebaseMessagingService {
+
+}
